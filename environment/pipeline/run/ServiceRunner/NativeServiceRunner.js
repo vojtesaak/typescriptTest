683d4c3258f61d41d5a78c8b8b514b6acf2a1146
Jaromir Franek <jaromir.franek@ibillboard.com>
2016-10-13 11:30:19 +0200

    + Doc to native service runner, run child node app from cwd

--- environment/pipeline/run/ServiceRunner/NativeServiceRunner.js
+++ environment/pipeline/run/ServiceRunner/NativeServiceRunner.js
@@ -3,13 +3,20 @@ const PathProvider = require('../../../utils/PathProvider');
 const child_process = require('child_process');
 const fs = require('fs');
 
+/**
+ * Run service as child process
+ * Known problem is that cwd is set to '/' instead of passed cwd
+ * @constructor
+ */
 function NativeServiceRunner (service) {
 	const distPath = PathProvider.getDistPath(service.getServiceName());
 	const stdout = fs.openSync(PathProvider.join(distPath, 'console.log'), 'a');
 	const stderr = fs.openSync(PathProvider.join(distPath, 'err.log'), 'a');
 
+	let childProcess = null;
+
 	this.run = function() {
-        child_process.spawn(`node`, [PathProvider.join(distPath, 'bundle.js')], {cwd: distPath, end: process.env, stdio: ['ignore', stdout, stderr]});
+		childProcess = child_process.spawn(`node`, ['bundle.js'], {cwd: distPath, env: process.env, stdio: ['ignore', stdout, stderr]});
 	};
 }
 
